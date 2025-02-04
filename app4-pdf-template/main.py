bbb6c69ee3123d7fc1ca6eb40e9dea17da7b3454
Roger Torkelsen <roger.torkelsen@miles.no>
2025-02-04 13:13:50 +0100

    Added multiple pages pr. topic with footers

--- app4-pdf-template/main.py
+++ app4-pdf-template/main.py
@@ -4,7 +4,7 @@ from fpdf import FPDF
 df = pd.read_csv('topics.csv')
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False, margin=0)
 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -13,6 +13,18 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(x1=10,y1=21,x2=200,y2=21)
 
+    #Set the footer
+    pdf.ln(265) #breakline
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+        # Set the footer
+        pdf.ln(277)  # breakline
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 
 pdf.output("output.pdf")
