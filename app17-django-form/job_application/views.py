f50fcc4400e82f55ce81b3f5eefcbea3c51cdbd2
Roger Torkelsen <roger.torkelsen@miles.no>
2025-02-21 12:25:50 +0100

    High-level gmail support via django.core.mail

--- app17-django-form/job_application/views.py
+++ app17-django-form/job_application/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Form
 from django.contrib import messages
+from django.core.mail import EmailMessage
 
 
 def index(request):
@@ -19,6 +20,11 @@ def index(request):
                                 email=email,
                                 date=date,
                                 occupation=occupation)
+
+            message_body = f"A new job application was submitted. Thank you, {first_name}."
+            email_message = EmailMessage(subject="From submission confirmation", body=message_body, to=[email])
+            email_message.send()
+
             messages.success(request, "From submitted successfully!")
 
     return render(request, "index.html")
